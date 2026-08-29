bc1bb9977dcba17ef6d874966ab05c69d13da82b
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2026-08-29 12:31:00 -0500

    Add author Controller

--- SA.APILibrary/Program.cs
+++ SA.APILibrary/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Area de servicios
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+//Area de middlewares
+app.MapControllers(); //le estamos diciendo que usemos los controladores para dar respuestas a las peticiones que lleguen a la API
 
 app.Run();

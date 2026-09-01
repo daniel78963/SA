9d72a9b6e3cc9a6efebbc4509f6f34c74c6344c1
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2026-09-01 13:01:37 -0500

    SA API - CRUD  pt1 - get list, get ID, save

--- SA.APILibrary/Controllers/AuthorController.cs
+++ SA.APILibrary/Controllers/AuthorController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SA.APILibrary.Data;
 using SA.APILibrary.Entities;
 
 namespace SA.APILibrary.Controllers
@@ -7,14 +9,33 @@ namespace SA.APILibrary.Controllers
     [Route("api/authors")]
     public class AuthorController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<Author> Get()
+        private readonly ApplicationDbContext context;
+
+        public AuthorController(ApplicationDbContext context)
         {
-            return new List<Author> 
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Author>> Get()
+        {
+            //return new List<Author>
+            //{
+            //    new Author { Id = 1, Name = "Author 1" },
+            //    new Author { Id = 2, Name = "Author 2" }
+            //};
+            return await context.Authors.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Author>> Get(int Id)
+        {
+            var author = await context.Authors.FirstOrDefaultAsync(x => x.Id == Id);
+            if (author is null)
             {
-                new Author { Id = 1, Name = "Author 1" },
-                new Author { Id = 2, Name = "Author 2" }
-            };
+                return NotFound();
+            }
+            return Ok(author);
         }
 
         [HttpGet("api/authors")]
@@ -32,6 +53,17 @@ namespace SA.APILibrary.Controllers
             var author = $"Author {id}";
             return Ok(author);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Author author)
+        {
+            // Logic to create a new author
+            //return CreatedAtAction(nameof(GetAuthorById), new { id = author.Id }, author);
+            context.Add(author);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
     }
     //{
     //    public IActionResult Index()

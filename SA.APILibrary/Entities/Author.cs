c02a9aae99620353ef52f49c0c060c9438155207
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2026-09-11 18:12:39 -0500

    SA API Validation by Model

--- SA.APILibrary/Entities/Author.cs
+++ SA.APILibrary/Entities/Author.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SA.APILibrary.Entities
 {
-    public class Author
+    public class Author : IValidatableObject
     {
         public int Id { get; set; }
         [Required]
@@ -22,5 +22,23 @@ namespace SA.APILibrary.Entities
         [Url]
         public string? URL { get; set; }
 
+        /// <summary>
+        /// Las validaciones por modelo son utiles para combinar campos o que requieren una lógica más compleja.
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+
+                if (firstLetter != firstLetter.ToUpper())
+                //if (firstLetter != firstLetter.ToUpper() && Age > 40)
+                {
+                    yield return new ValidationResult("The first letter must be uppercase - by model", new[] { nameof(Name) });
+                }
+            }
+        }
     }
 }

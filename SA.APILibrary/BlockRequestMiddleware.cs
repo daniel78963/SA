0adacf5751961a417401bcc75b60c1a5be7fe789
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2026-09-18 18:44:26 -0500

    SA API BlockRequestMiddleware

---
+++ SA.APILibrary/BlockRequestMiddleware.cs
@@ -0,0 +1,33 @@
+﻿namespace SA.APILibrary
+{
+    public class BlockRequestMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public BlockRequestMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path == "/bloqueado")
+            {
+                context.Response.StatusCode = 403; // Forbidden
+                await context.Response.WriteAsync("Access to this resource is forbidden.");
+            }
+            else
+            {
+                await next(context);
+            }
+        } 
+    }
+
+    public static class BlockRequestMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBlockRequestMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BlockRequestMiddleware>();
+        }
+    }
+}
